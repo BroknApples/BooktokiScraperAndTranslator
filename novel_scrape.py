fe646866408f47f373bca6a8135081d4162cdd40
Brokn_Apples <trentons708@gmail.com>
2025-07-05 20:41:36 -0500

    Main method updated

--- novel_scrape.py
+++ novel_scrape.py
@@ -13,7 +13,6 @@ from src.scraper import Scraper
 from src.translator import TextTranslator
 
 
-
 # TODO: Try deep-translator for translation when done
 
 # === Function: main ===
@@ -27,6 +26,10 @@ async def main() -> None:
   # Print some whitespace before starting
   print()
 
+  # Setup directory if not already
+  OUTPUT_DIRECTORY_ROOT: str = "translations/" # Directory that novels will be outputted to
+  os.makedirs(OUTPUT_DIRECTORY_ROOT, exist_ok=True)
+
   running: bool = True # Is the application running
   scraper: Scraper = Scraper()
   translator: TextTranslator = TextTranslator()
@@ -39,8 +42,80 @@ async def main() -> None:
   # translated_text: str = translator.translateString(untranslated_text)
   # print("The translated text is: " + translated_text)
 
-  while (running):
-    url = input("Input the chapter URL: ")
+  # TODO: Review this entire while loop and change to a ui-based version
+  #       better yet, just put this entire while loop inside the ui.py file
+  #       and have a class that accepts the translator/scraper as parameters
+  #       for functions or something idk...
+  while running:
+    # Get the novel URL
+    novel_url: str = input("Enter the novel URL: ")
+
+    # Get the starting chapter index
+    start_idx = "Uninitialized"
+    while not start_idx.isdigit() and start_idx != "":
+      start_idx = ""
+      start_idx = input("Enter the starting chapter(Press ENTER for Chapter 1): ")
+    if start_idx == "":
+      start_idx = "1"
+    start_idx = int(start_idx)
+    
+    # Get the ending chapter 
+    end_idx = "Uninitialized"
+    while not end_idx.isdigit() and end_idx != "":
+      end_idx = ""
+      end_idx = input("Enter the ending chapter(Press ENTER for the Latest Chapter): ")
+    if end_idx == "":
+      end_idx = "2147000000"
+    end_idx = int(end_idx)
+      
+    # Get the directory to save files to
+    output_directory: str = ""
+    while (output_directory == ""):
+      output_directory = input("Enter a name for your output directory: ")
+    output_directory = OUTPUT_DIRECTORY_ROOT + output_directory
+    
+    # TODO: Do actual work here
+    start = input("Start scrape? (y/n): ")
+    if (start == y):
+      # Scrape the novel
+
+      # Initialize values in the scraper
+      scraper.setNovelChapterListUrl(novel_url)
+
+      # Initialize values in the translator
+      # TODO: MAKE THESE ACTUALLY CHANGEABLE
+      translator.setSourceLanguage(TextTranslator.Languages.KOREAN)
+      translator.setDestinationLanguage(TextTranslator.Languages.ENGLISH)
+
+      # Start the scrape
+      chapter_data: list[str] = scraper.scrape(start_idx=start_idx, end_idx=end_idx)
+
+      # Replace weird ellipses characters with actual periods
+
+      # Translate the data
+      translation_data: list[str] = translator.translateStringArray(chapter_data)
+      
+      # Create the directory to save to
+      os.makedirs(output_directory)
+
+      # Save the translated data to the disk
+      CHAPTER_NAME: str = "Chapter " # NOTE: Appends the number to the end when using
+      array_index: int = 0 # Used to actually index the array
+      for i in range(start_idx, end_idx):
+        # Create chapter name
+        curr_chapter_name: str = CHAPTER_NAME + i + ".txt"
+
+        # Write to file
+        with open(output_directory + "/" + curr_chapter_name, "w", encoding="utf-8") as f:
+          f.write(translation_data[array_index])
+        
+        # Increment array index
+        array_index += 1
+    
+    continue_choice = input("Continue? (y/n): ")
+
+    if (continue_choice != "y"):
+      running = false
 
 
 # Run the main script
